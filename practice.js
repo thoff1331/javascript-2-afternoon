0934daf970cad027ec26c60930066c3d63f7fa1f
Trevor hoffman <trevorhoffman@Trevors-MacBook-Air.local>
2019-03-13 15:36:08 -0500

    finished

--- practice.js
+++ practice.js
@@ -14,8 +14,11 @@
   Then alert your name using dot notation.
 */
 
-//Code here
-
+let me = {
+  name: 'Trevor',
+  age: 25
+}
+alert(me.name)
 
 
 ////////// PROBLEM 2 //////////
@@ -25,7 +28,15 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
+let favoriteThings = {
+  band: 'Blue October',
+  food: 'Burritos',
+  person: 'Meg',
+  movie: 'titanic',
+  book: 'Glory Road',
+  holiday: 'Halloween'
+
+}
 
 
 
@@ -33,7 +44,8 @@
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = 'Tacoma';
+favoriteThings.brand = 'nike'
 
 
 
@@ -41,7 +53,8 @@
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = 'Chicken Nuggets'
+favoriteThings.book = 'Harry Potter'
 
 
 
@@ -55,15 +68,19 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
+let backPack = {
 
-
+}
+let item =  'firstPocket';
+backPack.firstPocket = item
+backPack.firstPocket = 'chapstick'
+backPack.color = 'blue'
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+alert(backPack);
 
 
 
@@ -73,7 +90,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
+console.log(backPack)
 
 
 
@@ -96,7 +113,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = 'Tyler S. McGinnis'
+user2.email = 'tyler.mcginnis@devmounta.in'
 
 
 
